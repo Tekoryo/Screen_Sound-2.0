88f15a8abd23613e9abae8ba155f098738e98aab
Thiago Coelho <thiagocoelho.coelho2@gmail.com>
2023-08-23 19:55:04 -0300

    Melhorando a class PodCast

--- Podcast.cs
+++ Podcast.cs
@@ -8,16 +8,10 @@ class Podcast{
     }
     public string Host {get;}
     public string Nome {get;}
-    public int TotalEpisodio = -1;
+    public int TotalEpisodio => Eps.Count();
     public void AdicionarEpisodio(Episodio eps)
     {
-        Eps.Add(eps);
-        
-        foreach(var episodio in Eps)
-        {
-            TotalEpisodio++;
-        }
-        
+        Eps.Add(eps);       
     }
 
     public void ExibirDetalhe()
@@ -27,7 +21,7 @@ class Podcast{
         Console.WriteLine($"Total de Episodio: {TotalEpisodio} \n");
         Console.WriteLine($"Detalhe dos Episodio: \n");
 
-        foreach(var episodio in Eps)
+        foreach(var episodio in Eps.OrderBy(e => e.Ordem))
         {
             episodio.ExibirEpisodio();
         }
